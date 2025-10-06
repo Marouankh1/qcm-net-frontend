3f7726768aec93eb0e47fb65f8ca5faf092360d1
Marouankh1 <marouankhabali36@gmail.com>
2025-10-07 00:24:44 +0100

    feat(login): new UI design with backend integration [WIP]

---
+++ src/pages/auth/components/form-login.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { FieldSeparator } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useEffect } from 'react';
+import { useNavigate, Link } from 'react-router';
+import useAuthStore from '@/stores/authStore';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+// import { useLogin, useUser } from '@/hooks/useApi';
+
+const loginSchema = z.object({
+    email: z.email({ message: 'Invalid email address' }),
+    password: z
+        .string()
+        .min(1, {
+            message: 'Please enter your password',
+        })
+        .min(7, {
+            message: 'Password must be at least 7 characters long',
+        }),
+});
+
+function FormLogin() {
+    const { login, isLoading, error, clearError, user } = useAuthStore();
+    // const loginMutation = useLogin();
+    // const { data: user } = useUser();
+
+    const onSubmit = async (credentials) => {
+        console.log('onSubmit ===> credentials');
+        console.log(credentials);
+        const result = await login(credentials);
+        console.log('result');
+        console.log(result);
+        // console.log('user');
+        // console.log(user);
+        // const result = await loginMutation.mutateAsync(credentials);
+        // console.log('result');
+        // console.log(result);
+        // if (result.success) {
+        //     console.log('result.success');
+        // }
+    };
+    const navigate = useNavigate();
+
+    const form = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
+
+    // useEffect(() => {
+    //     return () => clearError();
+    // }, [clearError]);
+
+    // const onSubmit = async (data) => {
+    //     console.log(data);
+    //     // clearError();
+    //     // const result = await login(data);
+
+    //     // if (result.success) {
+    //     //     console.log('Login successful');
+    //     // }
+    // };
+
+    const handleGoogleLogin = () => {
+        console.log('Google login clicked');
+    };
+
+    return (
+        <div className="w-full max-w-sm mx-auto space-y-6">
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-4">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem className="space-y-2">
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="email"
+                                        placeholder="name@example.com"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem className="space-y-2">
+                                <div className="flex items-center justify-between">
+                                    <FormLabel>Password</FormLabel>
+                                    {/* Uncomment if you have forgot password feature */}
+                                    {/* <Link
+                                        to="/forgot-password"
+                                        className="text-sm font-medium text-muted-foreground hover:opacity-75"
+                                    >
+                                        Forgot password?
+                                    </Link> */}
+                                </div>
+                                <FormControl>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        placeholder="********"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button
+                        type="submit"
+                        className="w-full cursor-pointer disabled:cursor-none"
+                        disabled={isLoading}>
+                        {isLoading ? 'Logging in...' : 'Login'}
+                    </Button>
+                </form>
+            </Form>
+
+            {/* <FieldSeparator className="my-4">Or continue with</FieldSeparator>
+
+            <Button
+                onClick={handleGoogleLogin}
+                variant="outline"
+                type="button"
+                className="w-full flex items-center justify-center gap-3">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="w-5 h-5">
+                    <path
+                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                        fill="currentColor"
+                    />
+                </svg>
+                Login with Google
+            </Button> */}
+        </div>
+    );
+}
+
+export default FormLogin;
