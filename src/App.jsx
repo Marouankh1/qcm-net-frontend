a537cba224f60988ec5097f13f9e7e72497d5529
Marouan Khabali <marouankhabali36@gmail.com>
2025-10-07 18:57:35 +0100

    feat: integrate a layout for app (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/sonner';
 import { useLogout } from '@/hooks/react-query/auth/useAuth';
 import useAuthStore from '@/stores/authStore';
-import { useNavigate } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import { toast } from 'sonner';
 // import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 // import { AppSidebar } from '@/components/app-sidebar';
@@ -37,13 +48,54 @@ function App() {
                 richColors
                 toastOptions={{}}
             />
-            Welcome To QCM-NET Platform
+            <SidebarProvider>
+                <AppSidebar />
+                <main>
+                    <SidebarTrigger />
+                    <Outlet />
+                </main>
+            </SidebarProvider>
+
+            {/* <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                        <div className="flex items-center gap-2 px-4">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator
+                                orientation="vertical"
+                                className="mr-2 data-[orientation=vertical]:h-4"
+                            />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </div>
+                    </header>
+                    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                            <div className="bg-muted/50 aspect-video rounded-xl" />
+                            <div className="bg-muted/50 aspect-video rounded-xl" />
+                            <div className="bg-muted/50 aspect-video rounded-xl" />
+                        </div>
+                        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+                    </div>
+                </SidebarInset>
+            </SidebarProvider> */}
+            {/* Welcome To QCM-NET Platform
             <Button
                 className="cursor-pointer disabled:cursor-none"
                 disabled={isLoading}
                 onClick={() => handleLogOut()}>
                 {isLoading ? 'Logout ...' : 'Logout'}
-            </Button>
+            </Button> */}
         </>
     );
 }
